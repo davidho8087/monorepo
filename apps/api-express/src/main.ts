26bd4cb6bc24f26ba013f441971c1647cf618fe5
David Ho <1073113@gmail.com>
2022-02-07 16:23:54 +0800

    express-graphql

--- apps/api-express/src/main.ts
+++ apps/api-express/src/main.ts
@@ -4,22 +4,96 @@
  */
 
 import * as express from 'express';
+import * as http from 'http';
 import cors = require('cors');
 import { restaurants } from './resto-data';
-const app = express();
 
-app.use(cors());
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api-express!' });
-});
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+    restaurants: [Restaurant]
+  }
 
-app.get('/api/restaurant', (req, res) => {
-  res.send(restaurants);
-});
+  type Restaurant {
+    id: Int
+    name: String
+    neighborhood: String
+    photograph: String
+    address: String
+    latlng: Latlng
+    cuisine_type: String
+    operating_hours: OperatingHours
+    reviews: [Review]
+  }
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+  type Latlng {
+    lat: Int
+    lng: Int
+  }
+
+  type OperatingHours {
+    Monday: String
+    Tuesday: String
+    Wednesday: String
+    Thursday: String
+    Friday: String
+    Saturday: String
+    Sunday: String
+  }
+
+  type Review {
+    name: String
+    date: String
+    rating: Int
+    comments: String
+  }
+`;
+
+// Return a list of users
+const getRestaurants = function () {
+  console.log('the resto', restaurants);
+  return restaurants;
+};
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    restaurants: getRestaurants,
+  },
+};
+
+async function listen(port: number) {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.use(cors());
+  return new Promise((resolve, reject) => {
+    httpServer.listen(port).once('listening', resolve).once('error', reject);
+  });
+}
+
+async function main() {
+  try {
+    await listen(4000);
+    console.log('Server is ready at http://localhost:4000/graphql');
+  } catch (err) {
+    console.error(' Error starting the node server', err);
+  }
+}
+
+void main();
